2b1d83947af6a041178b92e917c2750066f10e91
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-12-30 12:20:12 -0500

    Add all my files

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "tachyons";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{ overflow: "scroll", border: "5px solid black", height: "800px" }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
